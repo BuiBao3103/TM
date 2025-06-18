c1e086f072670a0fbeef60af78e9bb9de5a842f4
duongminhtuan <duongminhtuan552@gmail.com>
2025-06-18 15:21:35 +0700

    create viewModel for update passenger

---
+++ TM/Models/ViewModels/TourPassengerUpdateViewModel.cs
@@ -0,0 +1,21 @@
+﻿
+namespace TM.Models.ViewModels
+{
+    public class TourPassengerUpdateViewModel
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string FullName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string IdentityNumber { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int TourId { get; set; }
+        public decimal AssignedPrice { get; set; }
+        public decimal CustomerPaid { get; set; }
+        public string Status { get; set; }
+        public string TourName { get; set; }
+    }
+}
