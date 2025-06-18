6bbb801a4c1d18163fc9a02634564c9d64454384
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2025-06-18 14:36:51 +0700

    update + delete surcharge

---
+++ TM/Controllers/TourSurchargeController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TM.Models;
+using TM.Models.ViewModels;
+
+namespace TM.Controllers
+{
+    public class TourSurchargeController : Controller
+    {
+        AppDbContext _appDbContext;
+
+        public TourSurchargeController(AppDbContext dbContext)
+        {
+            _appDbContext = dbContext;
+        }
+
+        [HttpGet("tour-surcharge/update")]
+        public async Task<IActionResult> UpdateAsync([FromQuery] int id)
+        {
+            try
+            {
+                var oldSucharge = await _appDbContext.TourSurcharges.FindAsync(id);
+
+                if (oldSucharge == null)
+                {
+                    ViewBag.ErrorMessage = "Không tìm thấy phụ thu với ID đã nhập.";
+                    return View("UpdateSurcharge");
+                }
+
+                var suchargeViewModel = new TourSurchargeUpdateViewModel { Id = id, Name = oldSucharge.Name, Amount = oldSucharge.Amount };
+
+                return View("UpdateSurcharge", suchargeViewModel);
+            } catch (Exception)
+            {
+                ViewBag.ErrorMessage = "Đã xảy ra lỗi.";
+                return View("UpdateSurcharge");
+            }
+        }
+
+        [HttpPost("tour-surcharge/update")]
+        public async Task<IActionResult> UpdateAsync(TourSurchargeUpdateViewModel tourSurchangeUpdate)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View("UpdateSurcharge", tourSurchangeUpdate);
+                }
+
+                var oldSuchange = await _appDbContext.TourSurcharges.FindAsync(tourSurchangeUpdate.Id);
+
+                if (oldSuchange == null)
+                {
+                    ViewBag.ErrorMessage = "Không tìm thấy phụ thu với ID đã nhập.";
+                    return View("UpdateSurcharge");
+                }
+
+                oldSuchange.Amount = tourSurchangeUpdate.Amount;
+                oldSuchange.Name = tourSurchangeUpdate.Name;
+                await _appDbContext.SaveChangesAsync();
+
+                return Redirect("/Tour/Surcharges/" + oldSuchange.TourId);
+            } catch (Exception)
+            {
+                ViewBag.ErrorMessage = "Đã xảy ra lỗi.";
+                return View("UpdateSurcharge", tourSurchangeUpdate);
+            }
+        }
+
+        [HttpPost("tour-surchange/delete")]
+        public async Task<IActionResult> Delete([FromForm] int id)
+        {
+            try
+            {
+                var oldSuchange = await _appDbContext.TourSurcharges.FindAsync(id);
+
+                if (oldSuchange == null || oldSuchange.DeleteAt != null)
+                {
+
+                    ViewBag.ErrorMessage = "Không tìm thấy phụ thu với ID đã nhập.";
+                    return Redirect("/Tour");
+                }
+
+                oldSuchange.DeleteAt = DateTime.Now;
+                await _appDbContext.SaveChangesAsync();
+
+                ViewBag.SuccessMessage = "Xóa thành công.";
+                return Redirect("/Tour/Surcharges/" + oldSuchange.TourId);
+            } catch (Exception)
+            {
+                ViewBag.ErrorMessage = "Đã xảy ra lỗi.";
+                return Redirect("/Tour");
+            }
+        }
+    }
+}
---
+++ TM/Models/ViewModels/TourSurchargeUpdateViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TM.Models.ViewModels
+{
+    public class TourSurchargeUpdateViewModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required decimal Amount { get; set; }
+    }
+}
