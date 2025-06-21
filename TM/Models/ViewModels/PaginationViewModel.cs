e343de3b656c10e6ba68044839a01c170cb13a8f
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2025-06-21 15:53:02 +0700

    add paging UI

---
+++ TM/Models/ViewModels/PaginationViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TM.Models.ViewModels
+{
+    public class PaginationViewModel
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int[] PageSizeOptions { get; set; } = [10, 20, 50, 100];
+        public required string ActionName { get; set; }
+    }
+}
