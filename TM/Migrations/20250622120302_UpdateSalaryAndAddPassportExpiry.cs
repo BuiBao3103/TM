80c001d5c67127aed845e689a5613509034589a4
BuiBao3103 <hongbao2003@gmail.com>
2025-06-22 19:05:44 +0700

    Migration Update Salary And Add PassportExpiry

---
+++ TM/Migrations/20250622120302_UpdateSalaryAndAddPassportExpiry.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TM.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSalaryAndAddPassportExpiry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "TourSurcharge",
+                type: "decimal(38,0)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,0)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SuggestPrice",
+                table: "Tour",
+                type: "decimal(38,0)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,0)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "HhFee",
+                table: "Tour",
+                type: "decimal(38,0)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,0)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DiscountPrice",
+                table: "Tour",
+                type: "decimal(38,0)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,0)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CustomerPaid",
+                table: "Passenger",
+                type: "decimal(38,0)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,0)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AssignedPrice",
+                table: "Passenger",
+                type: "decimal(38,0)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,0)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "PassportExpiryDate",
+                table: "Passenger",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PassportExpiryDate",
+                table: "Passenger");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "TourSurcharge",
+                type: "decimal(18,0)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,0)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SuggestPrice",
+                table: "Tour",
+                type: "decimal(18,0)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,0)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "HhFee",
+                table: "Tour",
+                type: "decimal(18,0)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,0)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DiscountPrice",
+                table: "Tour",
+                type: "decimal(18,0)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,0)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CustomerPaid",
+                table: "Passenger",
+                type: "decimal(18,0)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,0)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AssignedPrice",
+                table: "Passenger",
+                type: "decimal(18,0)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,0)",
+                oldNullable: true);
+        }
+    }
+}
