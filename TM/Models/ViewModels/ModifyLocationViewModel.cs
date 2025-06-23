066a9809015e45484c830eaf65c2e2d87a86326f
Nguyễn Đức Nhật Hào <thaingoc924282@gmail.com>
2025-06-23 13:05:53 +0700

    update add location

---
+++ TM/Models/ViewModels/ModifyLocationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using TM.Models.Entities;
+
+namespace TM.Models.ViewModels
+{
+    public class ModifyLocationViewModel
+    {
+        public List<Location> Locations { get; set; }
+        public Location EditLocation { get; set; }
+        public PaginationViewModel Pagination { get; set; }
+        public string Query { get; set; }
+        public SelectList CountrySelectList { get; set; }
+    }
+}
